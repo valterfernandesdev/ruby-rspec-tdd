4044098c09b3a0a6abe0977c252bcc33650acc4b
Valter Fernandes <valterfernandes.dev@gmail.com>
2019-05-03 16:38:33 -0300

    finalizando branch

--- spec/matchers/classes/classes_spec.rb
+++ spec/matchers/classes/classes_spec.rb
@@ -2,7 +2,7 @@ require 'string_nao_vazia'
 
 describe 'Classes' do
   it 'be_instance_of' do
-    expect(10).to be_instance_of(Integer) # Exatamente a classe
+    # expect(10).to be_instance_of(Integer) # Exatamente a classe
   end
 
   it 'be_kind_of' do
